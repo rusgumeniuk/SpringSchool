4c0a4fd5fb6ffcd88748d4e03b4a22eadbdfe56a
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-05-27 21:22:31 +0300

    moved to schoolmodels (to avoid dependency cycle)

--- schoolmodels/src/main/java/com/example/Teacher.java
+++ schoolmodels/src/main/java/com/example/Teacher.java
@@ -4,15 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.JoinColumn;
-import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
-@Table(name = "students")
+@Table(name = "teachers")
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(value = "isDeleted", allowGetters = true)
 public class Teacher extends com.example.Entity {
@@ -20,21 +19,39 @@ public class Teacher extends com.example.Entity {
     @NotBlank
     private String fullName;
 
-    @NotBlank
+    @NotNull
     private TeacherRank teacherRank;
 
     @NotBlank
     private String cathedra;
 
-    @JoinColumn(name = "group_id")
+    @OneToOne(cascade = CascadeType.ALL)
     private Group mentored_group;
+
 
     public Teacher() {}
 
+    public Teacher(@NotBlank String fullName, @NotBlank TeacherRank teacherRank, @NotBlank String cathedra) {
+        this.fullName = fullName;
+        this.teacherRank = teacherRank;
+        this.cathedra = cathedra;
+    }
     public Teacher(@NotBlank String fullName, @NotBlank TeacherRank teacherRank, @NotBlank String cathedra, Group mentored_group) {
         this.fullName = fullName;
         this.teacherRank = teacherRank;
         this.cathedra = cathedra;
         this.mentored_group = mentored_group;
+    }
+
+    public void setMentored_group(Group mentored_group) {
+        if(mentored_group != null)
+            deleteMentoring();
+        this.mentored_group = mentored_group;
+    }
+    public void deleteMentoring(){
+        if(mentored_group != null){
+            mentored_group.setMentor(null);
+            mentored_group = null;
+        }
     }
 }
