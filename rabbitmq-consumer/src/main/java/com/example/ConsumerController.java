8d043be3795c54ded2a9908a5c821ebc29cdf513
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-06-09 00:34:36 +0300

    Added 2 handle methods. Added general method handeEvent(MyEvent).

--- rabbitmq-consumer/src/main/java/com/example/ConsumerController.java
+++ rabbitmq-consumer/src/main/java/com/example/ConsumerController.java
@@ -1,14 +1,12 @@
 package com.example;
 
-import com.example.listeners.events.GroupEvent;
-import com.example.listeners.events.StudentEvent;
-import com.example.messages.GroupMessage;
+import com.example.listeners.events.*;
 import com.example.messages.Message;
 import com.example.messages.MessageRepository;
-import com.example.messages.StudentMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -24,32 +22,31 @@ public class ConsumerController {
     @Autowired
     private MessageRepository msgRepo;
 
-    @EventListener({GroupEvent.class})
-    public void handleGroupEvt(GroupEvent evt)
+    @EventListener({StudentGroupTeacherEvent.class})
+    public void handleStudentGroupTeacherEvt(StudentGroupTeacherEvent evt)
     {
-        System.out.println("EVENT RECEIVED: " + evt.getMessage().getDescription());
-        GroupMessage message = evt.getMessage();
-        msgRepo.save(new GroupMessage(message.getDescription(), message.getHttpMethod(), message.getStatusCode(), message.getDateTime(), message.getError()));
-        List<SseEmitter> deadEmitters = new ArrayList<SseEmitter>();
-        this.lsEmitters.forEach(emitter -> {
-            try {
-                emitter.send(message);
-            }
-            catch (Exception e) {
-                LOGGER.error("Error ",e);
-                deadEmitters.add(emitter);
-            }
-        });
-
-        this.lsEmitters.removeAll(deadEmitters);
+        handleEvent(evt);
+    }
+    @EventListener({SystemEvent.class})
+    public void handleSystemEvt(SystemEvent evt)
+    {
+       handleEvent(evt);
+    }
+    @EventListener({LesSubEvent.class})
+    public void handleLesSubEvt(LesSubEvent evt)
+    {
+        handleEvent(evt);
+    }
+    @EventListener({ArchEvent.class})
+    public void handleArchEvt(ArchEvent evt)
+    {
+        handleEvent(evt);
     }
 
-    @EventListener({StudentEvent.class})
-    public void handleStudentEvt(StudentEvent evt)
-    {
-        System.out.println("EVENT RECEIVED: " + evt.getMessage().getDescription());
-        StudentMessage message = evt.getMessage();
-        msgRepo.save(new StudentMessage(message.getDescription(), message.getHttpMethod(), message.getStatusCode(), message.getDateTime(), message.getError()));
+    private void handleEvent(MyEvent event){
+        System.out.println("EVENT RECEIVED: " + event.getMessage().getDescription());
+        Message message = event.getMessage();
+        msgRepo.save(new Message(message.getClassName(), message.getDescription(), message.getHttpMethod(), message.getStatusCode(), message.getDateTime(), message.getError()));
 
         List<SseEmitter> deadEmitters = new ArrayList<SseEmitter>();
         this.lsEmitters.forEach(emitter -> {
